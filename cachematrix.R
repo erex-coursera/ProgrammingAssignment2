020fcfea77d27e3c75a7e5647f5e3e2c49ed4214
erex-coursera <eric.rexstad@st-andrews.ac.uk>
2014-04-14 12:17:16 +0100

    Coded matrix inversion caching and comments
    With initial fork of Peng's skeleton, added code based upon template provided within instructions for the assignment that cached results from function "mean".

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,51 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions to cache results of computations,
+##     specifically, this set of functions cache
+##     result of matrix inversion
+##
+##     Written 14 April 2014 as part of Corsera R Programming course
+##
+##     Skeleton forked from repository rdpeng/ProgrammingAssignment2
 
-## Write a short comment describing this function
+## Function builds skeleton of matrix inversion caching
+##
+##     Input an assumed square, invertible matrix (no checking of input performed
+##     Output  list of 4 functions: get/set pair and getinv/setinv pair
+##
+##    Note side-effect use of '<<-' operator outisde of "normal" scoping practice
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+##      manufacture list of functions needed to cache matrix inversion
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinv <- function(solve) m <<- solve
+  getinv <- function() m
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## Either performs matrix inversion or returns result of cached solution if
+##     inversion has previously been carried out for argument
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getinv()
+##    Two cases depending upon m; if m is not NULL then calculation
+##          on argument has previously been performed, result is in x$getinv
+  if(!is.null(m)) {
+    message("Getting cached inversion solution")
+    return(m)
+  }
+##    Otherwise, perform inversion computation on data and indicate through
+##      setinv() function that an inverse for argument has been calculated
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinv(m)
+  return(m)
 }
